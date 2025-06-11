2df912b4e54acb96919c65d82677a26ae42722b1
jeong gil hun <wjdrlfgns1@naver.com>
2025-06-11 16:06:14 +0900

    논리연산자 실습

--- source/ch03.sql
+++ source/ch03.sql
@@ -96,6 +96,7 @@ SELECT TRUE  OR FALSE; -- 1
 SELECT FALSE OR TRUE; -- 1
 SELECT FALSE OR FALSE;-- 0
 
+
 -- 5500원 보다 싸고, 동시에 단백질량이 25g 보다 많은 버거
   SELECT *
   FROM burgers
@@ -108,4 +109,161 @@ SELECT FALSE OR FALSE;-- 0
 
   SELECT *
   FROM burgers
-  WHERE NOT protein>25;
+  WHERE NOT (protein>25); --혼동 방지를 위해 괄호를 명시하는 것도 좋음
+  -- WHERE  !(protein>25); --MySQL에서만 작동
+  --가독성과 이식성을 위해서는 NOT을 사용하는 것이 가장 안전
+
+
+  --3)산술 연산자
+  -- 사칙연산 등을 위한 수학적 연산 기호 
+  -- WHERE절과 SELECT 절에서 사용가능
+  SELECT 100+20;
+  SELECT 100-20;
+  SELECT 100*20;
+  SELECT 100/20;
+  SELECT 100%20;
+
+  --산술 연산자 활용 예
+  -- SELECT 문에서의 산술 연산 예시
+  SELECT *, price/gram*100 AS 'price/100g' -- 따옴표("",'') 또는 백틱(``)으로 감싸기
+  FROM burgers;
+  -- 100g당 가격을 계산하여 price/100g 이라는 별칭(alias)으로 반환함
+
+  -- WHERE 절에서 산술 연산 예시
+  -- 가격에 10%를 더한 값이 6500원을 넘는 버거만 조회
+
+  SELECT *
+  FROM burgers
+  WHERE price*1.1 >6500;
+
+
+    SELECT *
+  FROM burgers
+  WHERE id%2=0;
+
+--UPDATE 문에서 산술 연산 예시
+-- id가 5인 버거의 가격을 500원 인하
+
+UPDATE burgers
+SET price = price - 500
+WHERE id=5;
+
+-- 4) 연산자의 우선순위
+-- 어떤 연산자를 먼저 수행할 것인가의 기준
+-- 우선순위가 높은 것부터 낮은 순으로 수행
+-- 암기x (쓰다 보면 자연스럽게 익혀짐)
+-- 애매하면 최우선 순위 () 를 사용
+
+-- Quiz: 다음 쿼리의 수행 결과는?
+SELECT 3 + 5 * 2; -- 13
+SELECT (3 + 5) * 2; -- 16
+SELECT TRUE OR TRUE AND FALSE; --TRUE
+SELECT (TRUE OR TRUE) AND FALSE; -- FALSE
+
+-- Quiz
+-- 1. 다음 빈칸에 들어갈 용어는? (입력 예: ㄱㄴㄷㄹㅁ)
+-- ① __________: 테이블 조회 시 필터링 조건을 작성하기 위해 사용하는 명령 
+-- ② __________: 같지 않음을 비교하는 연산자
+-- ③ __________: 두 조건을 동시에 만족하는 논리 연산자
+-- ④ __________: 두 조건 중 하나라도 만족하는 논리 연산자
+-- ⑤ __________: 특정 조건을 만족하지 않는 논리 연산자
+
+-- (ㄱ) AND
+-- (ᄂ) !=
+-- (ᄃ) NOT
+-- (ᄅ) WHERE
+-- (ᄆ) OR
+
+-- 정답: ㄹ ㄴ ㄱ ㅁ ㄷ 
+
+
+/*
+3.2 데이터 필터링 실습: 대학 DB
+*/
+
+-- Quiz: 대학 DB 만들기
+-- university DB 생성 및 진입
+
+
+-- students 테이블 생성
+-- 아이디(정수형 숫자)
+-- 닉네임(문자형: 최대 50자)
+-- 수학 성적(정수형 숫자)
+-- 영어 성적(정수형 숫자)
+-- 프로그래밍 성적(정수형 숫자)
+-- 기본키 지정: id
+
+CREATE DATABASE university;
+USE university;
+SELECT DATABASE();
+
+CREATE TABLE students(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50),
+  math INTEGER,
+  english INTEGER,
+  programing INTEGER
+);
+-- students 데이터 삽입
+INSERT INTO students(id, name, math, english, programing)
+VALUES
+	(1, 'Sparkles', 98, 96, 93),
+	(2, 'Soldier', 82, 66, 98),
+	(3, 'Lapooheart', 84, 70, 82),
+	(4, 'Slick', 87, 99, 98),
+	(5, 'Smile', 75, 73, 70),
+	(6, 'Jellyboo', 84, 82, 70),
+	(7, 'Bagel', 97, 91, 87),
+	(8, 'Queen', 99, 100, 88);
+
+ALTER TABLE students RENAME COLUMN name TO nickname;
+
+-- 확인
+
+-- 필터링 연습!
+
+-- 1. 모든 과목 성적이 90점 이상인 학생은?
+  SELECT *
+  FROM students
+  WHERE math>=90 AND english >=90 AND programing >=90;
+
+-- 2. 75점 미만이 하나라도 있는 학생은?
+  SELECT *
+  FROM students
+  WHERE math<=75 OR english <=75 OR programing <=75;
+
+-- 3. 모든 학생의 총점은?
+
+  SELECT * , math+english+programing AS '총점'
+  FROM students ;
+
+-- 4. 모든 학생의 평균은?
+  SELECT * , (math+english+programing)/3 AS '평균'
+  FROM students ;
+
+-- 5. 총점이 270 이상인 학생의 닉네임, 총점, 평균은?
+
+  SELECT nickname AS '닉네임',math+english+programing AS '총점' , (math+english+programing)/3 AS '평균'
+  FROM students 
+  WHERE math+english+programing>=270;
+
+  -- 별칭 부여하기 : AS 키워드
+  -- SELECT 컬럼명 AS 별칭
+  -- FROM 테이블명;
+
+  -- 별칭 사용 시, 공백 또는 특수 문자 등이 필요한 경우
+  -- 별칭을 따옴표("",'') 또는 백틱(``) 으로 감싸야함
+
+  -- Quiz
+-- 2. 다음 쿼리에 대한 설명으로 옳지 않은 것을 모두 고르면?
+SELECT id, nickname, (math + english + programming) / 3 AS '중간고사 평균' 
+FROM students
+WHERE math > 80 AND programming > 80 OR English > 90;
+
+-- ① students 테이블에서 데이터를 조회하고 있다.
+-- ② math가 80보다 높은 학생은 반드시 조회된다.
+-- ③ english가 90보다 높은 학생은 조회될 수 있다.
+-- ④ 이 쿼리를 실행하면 그 결과로 칼럼 3개가 조회된다.
+-- ⑤ SELECT 절의 '(math + english + programming) / 3' 수식에 괄호를 삭제하고 조회해도 정상적으로 중간고사 평균이 계산된다.
+
+-- 정답: 2 5
